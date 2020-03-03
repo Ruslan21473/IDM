4fbc41c1527a7ac4195be37dc06edc13e65d62e6
Ruslan21473 <54320118+Ruslan21473@users.noreply.github.com>
2020-03-03 17:57:49 +0600

    Update delete_messages_from_user.py

--- idm/commands/delete_messages_from_user.py
+++ idm/commands/delete_messages_from_user.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 @dp.event_handle(dp.Methods.DELETE_MESSAGES_FROM_USER)
 def delete_messages_from_user(event: Event) -> str:
-    message_id = utils.new_message(event.api, event.chat.peer_id, message="... удаляю сообщения ...")
+    message_id = utils.new_message(event.api, event.chat.peer_id, message="...удаляю сообщения...")
 
     user_id = event.obj['user_id']
     amount = event.obj.get("amount", None)
@@ -26,9 +26,9 @@ def delete_messages_from_user(event: Event) -> str:
         utils.edit_message(event.api, event.chat.peer_id, message_id, message="✅ Сообщения удалены")
     except VkApiResponseException as e:
         if e.error_code == 924:
-            utils.edit_message(event.api, event.chat.peer_id, message_id, message="❗ Не удалось удалить сообщения. Невозможно удалить сообщение, возможно пользователь администратор.")
+            utils.edit_message(event.api, event.chat.peer_id, message_id, message="❗ Не удалось удалить сообщения. Невозможно удалить сообщение, возможно пользователь администратор")
         else:
             utils.edit_message(event.api, event.chat.peer_id, message_id, message=f"❗ Не удалось удалить сообщения. Ошибка VK {e.error_msg}")
     except:
-        utils.edit_message(event.api, event.chat.peer_id, message_id, message=f"❗ Произошла неизвестная ошибка.")
+        utils.edit_message(event.api, event.chat.peer_id, message_id, message=f"❗ Произошла неизвестная ошибка ❗")
     return "ok"
